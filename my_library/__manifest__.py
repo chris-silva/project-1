eabcac3d67a526465d27d3e3350f4b4f5c8aaec8
csil300 <csilva@opensourceintegrators.com>
2022-01-31 15:40:56 -0700

    adding library

---
+++ my_library/__manifest__.py
@@ -0,0 +1,16 @@
+{
+    'name': 'My Library',
+    'summary': "Manage books easily",
+    'description': "Library App",
+    'author': "Your name",
+    'website': "http://www.example.com",
+    'category': 'Uncategorized',
+    'version': '13.0.1',
+    'depends': ['base', 'account'],
+    'data': [
+        'security/groups.xml',
+        'security/ir.model.access.csv',
+        'views/library_book.xml',
+        ],
+    
+}
